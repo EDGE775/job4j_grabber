f0bf52220001d9d78286bf0f9164aa98c6e0c225
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-07-26 19:49:20 +0300

    2.1. Преобразование даты [#289476 #181726] рефакторинг кода

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -3,39 +3,29 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 import java.util.Locale;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
-    private List<String> months = List.of("январь", "февраль", "март", "апрель",
-            "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь");
+    private static final Locale LOCALE = Locale.getDefault();
 
     @Override
     public LocalDateTime parse(String parse) {
         String date = parse.toLowerCase();
-        if (date.split(" ")[0].length() == 1) {
-            date = "0".concat(date);
-        }
         if (date.contains("сегодня")) {
             date = date.replace("сегодня", LocalDate.now()
-                    .format(DateTimeFormatter.ofPattern("dd LLLL yy")));
+                    .format(DateTimeFormatter.ofPattern("d MMM yy", LOCALE)));
         } else if (date.contains("вчера")) {
             date = date.replace("вчера", LocalDate.now()
                     .minusDays(1)
-                    .format(DateTimeFormatter.ofPattern("dd LLLL yy")));
+                    .format(DateTimeFormatter.ofPattern("d MMM yy", LOCALE)));
         } else {
-            for (int i = 0; i < months.size(); i++) {
-                String month = date.split(" ")[1];
-                if (months.get(i).contains(month)) {
-                    date = date.replace(month, months.get(i));
-                    break;
-                }
-            }
+            String[] dateArray = date.split(" ");
+            dateArray[1] = dateArray[1].concat(".");
+            date = String.join(" ", dateArray);
         }
         DateTimeFormatter dtf = DateTimeFormatter
-                .ofPattern("dd LLLL yy, HH:mm")
-                .withLocale(new Locale("ru"));
+                .ofPattern("d MMM yy, HH:mm", LOCALE);
         return LocalDateTime.parse(date, dtf);
     }
 }
