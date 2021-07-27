4a148140acf444b0cd25185aba53a1a6594b6bb3
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-07-27 22:11:14 +0300

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -32,7 +32,9 @@ public class Post {
     public Post() {
     }
 
-    public Post(String title, String link, String description, LocalDateTime created) {
+    public Post(int id, String title, String link,
+                String description, LocalDateTime created) {
+        this.id = id;
         this.title = title;
         this.link = link;
         this.description = description;
---
+++ src/main/java/ru/job4j/grabber/store/PsqlStore.java
@@ -0,0 +1,120 @@
+package ru.job4j.grabber.store;
+
+import ru.job4j.grabber.Post;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) throws SQLException {
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+        } catch (Exception e) {
+            throw new IllegalStateException("Драйвер DB не найден!", e);
+        }
+        cnn = DriverManager.getConnection(
+                cfg.getProperty("url"),
+                cfg.getProperty("username"),
+                cfg.getProperty("password"));
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "insert into post(name, text, link, created) values (?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getDescription());
+            statement.setString(3, post.getLink());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    post.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> posts = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    posts.add(new Post(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime()
+                    ));
+                }
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = null;
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post where id = ?"
+        )) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = new Post(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("text"),
+                            resultSet.getString("link"),
+                            resultSet.getTimestamp("created").toLocalDateTime());
+                }
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties props = null;
+        try (InputStream in = PsqlStore.class
+                .getClassLoader().getResourceAsStream("app.properties")) {
+            props = new Properties();
+            props.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (PsqlStore store = new PsqlStore(props)) {
+            Post post = new Post(
+                    1, "title", "link", "desc", LocalDateTime.now());
+            store.save(post);
+            System.out.println(store.getAll().get(0));
+            System.out.println(store.findById(post.getId()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
